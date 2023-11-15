5dfd36a578eb8054d4caa15110618d0df19bd471
Mio_02 <2570945644@qq.com>
2023-11-15 21:46:29 +0800

    '外包'

--- 外包/DreamCar/app.js
+++ 外包/DreamCar/app.js
@@ -1,19 +1,21 @@
 // app.js
+// 应用
+// page () 页面
+// 花括号内表示传的配置
 App({
+  globalData: {
+    
+  },
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    // 利用生命周期，在应用启动时，发送接口请求
+    // console.log('应用启动了');
+    wx.request({
+      url: 'https://resources.ninghao.net/wxapp-case/db.json',
+      success: (response) => {
+        // console.log(response);
+        Object.assign(this.globalData, response.data)
+        // console.log(this, '-----');
       }
     })
-  },
-  globalData: {
-    userInfo: null
   }
 })
