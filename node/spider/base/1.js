1b7dd2023811baad5a03e8bc70129f22da121e3a
Mio_02 <2570945644@qq.com>
2023-11-20 21:44:27 +0800

    'spider'

---
+++ node/spider/base/1.js
@@ -0,0 +1,15 @@
+// console.log('hello');
+// console.log(Data);
+// console.log(Math);
+
+// setTimeout(() => {
+//     console.log('hello');
+// }, 1000)
+
+// console.log(setInterval);
+// console.log(requestAnimationFrame); // 在浏览器上使用，用node报错
+
+// console.log(__dirname); // 查看文件夹绝对路径
+// console.log(__filename); // 查看文件绝对路径
+
+console.log(process);
