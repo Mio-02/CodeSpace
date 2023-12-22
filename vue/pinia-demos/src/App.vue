0da3a9d43b7922ce2ba3b2ba596fdc5d7b80d65e
Mio_02 <2570945644@qq.com>
2023-12-22 21:36:29 +0800

    pinia

---
+++ vue/pinia-demos/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// 以组件为开发单元
+import TodoList from './components/TodoList.vue'
+import {ref} from 'vue'
+// const todos = ref([
+//   {text: 'hello'}
+// ])
+// todos 来到 可以用vuex的mapState 或者pinia
+import { useTodoStore } from './store/todo'
+const todoStore = useTodoStore()
+const { todos, addTodo } = useTodoStore()
+// console.log(todoStore.todos);
+</script>
+
+<template>
+  <div>
+    <TodoList 
+      :todos="todoStore.todos"
+      :addTodo="todoStore.addTodo"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
