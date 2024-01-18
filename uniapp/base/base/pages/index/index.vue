a5a4aaff0cf3fd4e6272ba00edb1ba5b3550e588
Mio_02 <2570945644@qq.com>
2024-01-18 20:53:45 +0800

    uniapp-base

---
+++ uniapp/base/base/pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area" @click="changeTitle">
+			<text class="title">{{title}}</text>
+			<text>{{reverseTitle}}</text>
+		</view>
+		
+		<song></song>
+	</view>
+</template>
+
+<script setup>
+	import { computed, ref } from 'vue'
+	import song from '../../components/song/song.vue'
+	
+	const title = ref('你好')
+	
+	const changeTitle = () => {
+		title.value = title.value === '你好' ? 'Hello' : '你好'
+	}
+	
+	const reverseTitle = computed(() => {
+		return title.value.split('').reverse().join('')
+	})
+</script>
+
+<style>
+
+</style>
